abae1ab55c678949a8b693d2825e59af26724268
WilsonKoder111 <wilson.koder.111@gmail.com>
2014-12-13 11:10:08 +0800

    change to postgres for heroku + added some new features

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'ideas#index'
+  root to: 'ideas#index'
   resources :ideas
 
   # You can have the root of your site routed with "root"
